4524803bed8b135ae6f64be21e923c505854d1dc
BigBiba <nikitafedorov.or@gmail.com>
2023-05-06 17:19:57 +0300

    Исправлены ошибки

--- Assets/Observer.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Observer : MonoBehaviour
-{   
-    static List<string> defeatedEnemies = new List<string>();
-}
