5e5c941b7f7a42640e7769a87aab249b8f1132a4
leikocits <kupreev.k.161@gmail.com>
2023-05-06 15:28:49 +0300

    2

--- Assets/Sprites/Scripts/Collision.cs
+++ Assets/Sprites/Scripts/Collision.cs
@@ -1,17 +1,30 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
+=======
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+>>>>>>> Stashed changes
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    //public Observer obs;
+    //public  ObjectManager objectManager = ObjectManager.instance;
+>>>>>>> Stashed changes
     public GameObject player;
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Collision detected!");
         if (other.gameObject.tag == "Enemy")
         {
+<<<<<<< Updated upstream
 
             DontDestroyOnLoad(other.gameObject);
             DontDestroyOnLoad(gameObject);
@@ -19,6 +32,13 @@ public class Collision : MonoBehaviour
             ObjectManager.instance.player = gameObject;
             BattleSystem.playerPrefab = gameObject;
             BattleSystem.enemyPrefab = other.gameObject;
+=======
+            DontDestroyOnLoad(other.gameObject);
+            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(obs);
+            ObjectManager.instance.enemy = other.gameObject;
+            ObjectManager.instance.player = gameObject;
+>>>>>>> Stashed changes
             var a = metals1.metal1;
             var b = woods1.wood1;
             var c = rubers1.rubber1;
@@ -26,8 +46,16 @@ public class Collision : MonoBehaviour
             SaveLoadManager.BattleData.SurrogateVector3 position = this.transform.position;
             SaveLoadManager.BattleData Data = new SaveLoadManager.BattleData(a, b, c, d, position);
             SaveLoadManager.SaveButle(Data);
+<<<<<<< Updated upstream
             SceneManager.LoadScene(2);
         }
 
+=======
+            //BattleSystem.playerPrefab = gameObject;
+            //BattleSystem.enemyPrefab = other.gameObject;
+
+            SceneManager.LoadScene("Battle");
+        }
+>>>>>>> Stashed changes
     }
 }
